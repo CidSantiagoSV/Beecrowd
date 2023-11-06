13ec549258f54887a9db72a2e5ed8f102dc78c0e
Cid Vieira <cid.santiago1@gmail.com>
2023-11-06 11:29:24 -0600

    Exercise 1008.py

---
+++ Beginner/1008.py
@@ -0,0 +1,7 @@
+eNumber = int(input())
+eWorkedHours = int(input())
+hourlyAmount = float(input())
+payment = eWorkedHours * hourlyAmount
+
+print("NUMBER = %i" % eNumber)
+print("SALARY = U$ %.2f" % payment)
